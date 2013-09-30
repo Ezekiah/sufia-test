8cf10223bea01156191a26fe4105d92c88d26b83
Alessandro <alexandreaazzouz@gmail.com>
2013-09-30 15:44:24 +0200

    add all folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sufia::Application.config.secret_token = 'f6d5422096acc818c0162661874c50cd84b6afcaa2aa627723e05bd76e75ac851aa1ea5bdd3908e99c0809158400201705c10eed30e48d73fc9ecdf4cd9f1ab1'
